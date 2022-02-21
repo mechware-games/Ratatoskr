12fd432ed63e21a71f1c8dcfe8c59218481ab8c7
txeptsyip <lekoro@hotmail.co.uk>
2022-02-21 01:15:22 +0000

    moving platform!
    i got a platform to move you can make it do it once or do it repeatedly or immediately go back to the start!

---
+++ Assets/Scripts/Samuel Scripts/3testscripts/GameCommandType.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace moving.shit
+{
+    public enum GameCommandType
+    {
+        None,
+        Activate,
+        Deactivate,
+        Open,
+        Close,
+        Spawn,
+        Destroy,
+        Start,
+        Stop
+    }
+}
