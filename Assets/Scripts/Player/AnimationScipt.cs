89cf511a77ca8a2f1bff9e04b3c0c46b44c642fc
JaydenBurgess <92925272+JaydenBurgess@users.noreply.github.com>
2022-04-25 18:45:21 +0100

    Animation Script coded and final few animations added
    notes in the script will mention important details. falling and dying script not finished but will just need to detect GroundCheck and Fenrir respectively. death animation added. Please edit the script in the falling and death sections so it can detect the ground and Fenrir.

--- Assets/Scripts/Player/AnimationScipt.cs
+++ Assets/Scripts/Player/AnimationScipt.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class AnimationScipt : MonoBehaviour
 {
+    //Anim is the animator that the animations are being pulled from.
     public Animator anim;
-
-    // Update is called once per frame
+    //Used to show the direction of the player(may not be needed but may also help).
+    private Vector3 MoveDirection;
+    //displays the values to me for testing.
+    [SerializeField] private float MovingForBack;
+    [SerializeField] private float MovingLeftRight;
+    [SerializeField] private float JumpAndFall;
+    [SerializeField] private bool Dying;
+    [SerializeField] private float Falling;
+    [SerializeField] private bool IsGrounded;
+    [SerializeField] private GameObject Player = null;
+    [SerializeField] Transform GroundCheck;
+    [SerializeField] private float TestingMoreThings;
+    [SerializeField] private float IDonno;
+    
+    // Update is called once per frame.
+    void Start()
+    {
+        //gets the player
+        if (Player == null)
+        {
+            Player = GameObject.Find("Ratatoskr");
+        }
+        IDonno = TestingMoreThings;
+    }
     void Update()
     {
-        //Squirrel walking animation. Needs to be tied to momentum of the squirrel instead of direction
-        anim.SetFloat("Forwards", Input.GetAxis("Vertical"));
-        anim.SetFloat("Forwards", Input.GetAxis("Vertical"));
-        //Jumping below here, when jump is pressed, set Airborn to 1, when not touching the ground, set airborn to 2, when touching the ground, make airborn go from 2 to 0
-        //Dying, when dead, set Dying float to 1
+
+        run();
+        JumpingAndFalling();
+        Dead();
+
+        
+        
     }
+    private void run()
+    {
+        //Squirrel walking animation. need to figure out player velocity.
+        MovingForBack = Input.GetAxis("Vertical");
+        MovingLeftRight = Input.GetAxis("Horizontal");
+        JumpAndFall = Input.GetAxis("Jump");
+        TestingMoreThings = Player.transform.position.y;
+        //animation calls
+        anim.SetFloat("Forwards", Input.GetAxis("Vertical"));
+        anim.SetFloat("Turning", Input.GetAxis("Horizontal"));
+        
+    }
+    private void JumpingAndFalling()
+    { 
+        //Jumping animation
+        anim.SetFloat("Jumping", Input.GetAxis("Jump"));
+        //Falling animation, needs to be set so when he is on the floor it doesn't play but if he is in the air it plays.
+        if (TestingMoreThings < IDonno)
+        {
+            IsGrounded = true;
+            IDonno = TestingMoreThings;
+        }
+        else
+        {
+            IsGrounded = false;
+            IDonno = TestingMoreThings;
+        }
+        if (IsGrounded == true)
+        {
+            anim.SetBool("Falling", IsGrounded);
+        }
+        else
+        {
+            anim.SetBool("Falling", IsGrounded);
+        }
+
+    }
+    private void Dead()
+    {
+        //Dying, need to tie this bool to character contacting wolf
+        if (Dying == true)
+        {
+            anim.SetBool("Dying", Dying);
+        }
+        else
+        {
+            anim.SetBool("Dying", Dying);
+        }
+    }
+
 }
