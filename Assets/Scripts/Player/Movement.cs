53a9acbdb9ac35f879160d52d02230562d9e38c0
Sheepie <chrisparsonsm97@yahoo.co.uk>
2022-06-09 16:22:25 +0100

    altered fall damage death amount

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -228,7 +228,7 @@ public class Movement : MonoBehaviour
                         {
                             float downforce = Mathf.Abs(((_currentGravityModifier * Physics.gravity) + Physics.gravity).y);
                             Debug.Log($"Vertical velocity: {downforce}");
-                            if (Mathf.Abs(downforce) > 150)
+                            if (Mathf.Abs(downforce) > 120)
                             {
                                 _playerState = State.Grounded;
                                 GetComponent<Player>().Death();
