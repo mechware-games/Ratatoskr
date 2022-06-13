da2477b8603ff37c3b483be9bbf47326cdec10d1
Sheepie <chrisparsonsm97@yahoo.co.uk>
2022-06-13 17:13:28 +0100

    minor changes to fenrir to fix jittering bug

--- Assets/Scripts/Fenrir/FenrirScript.cs
+++ Assets/Scripts/Fenrir/FenrirScript.cs
@@ -110,13 +110,13 @@ public class FenrirScript : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
     {
+        _pauseTimer = _pauseLength;
+        _chaseTimer = _chaseLength;
+        _LastDistanceFromPlayer = (_player.position - transform.position).magnitude;
         if (!GetActive())
         {
             Despawn();
         }
-        _pauseTimer = _pauseLength;
-        _chaseTimer = _chaseLength;
-        _LastDistanceFromPlayer = (_player.position - transform.position).magnitude;
     }
 
 	private void OnEnable()
@@ -150,13 +150,6 @@ public class FenrirScript : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-        if(_player == null)
-        {
-            _player = GameObject.FindGameObjectWithTag("Player").transform;
-            _playerSpottedLocation = _player.position;
-            _LastDistanceFromPlayer = (_player.position - transform.position).magnitude;
-        }
-
         transform.LookAt(_player.position);
         if (GetActive())
         {
@@ -305,7 +298,7 @@ public class FenrirScript : MonoBehaviour
 	{
         currentSpeed = _baseSpeed;
         _pauseTimer = _pauseLength;
-        _currentState = State.Chasing;
+        _currentState = State.Waiting;
         SetActive(true);
         ToggleChildrenActive(true);
         GetComponent<SphereCollider>().enabled = true;
